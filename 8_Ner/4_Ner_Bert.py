96fe71921419571438b6295b8d45a09f2bc59e5c
honeyandme <2414342539@qq.com>
2023-03-22 23:56:28 +0800

    modify Bert_Ner

--- 8_Ner/4_Ner_Bert.py
+++ 8_Ner/4_Ner_Bert.py
@@ -9,22 +9,23 @@ from transformers import logging
 
 logging.set_verbosity_warning()
 def get_data(path):
-    all_text,all_tag = [],[]
+    all_text_tag = []
     with open(path,'r',encoding='utf8') as f:
         all_data = f.read().split('\n')
-
     sen,tag = [],[]
     for data in all_data:
         data = data.split(' ')
         if(len(data)!=2):
             if len(sen)>2:
-                all_text.append(sen)
-                all_tag.append(tag)
+                all_text_tag.append((sen,tag))
             sen, tag = [], []
             continue
         te,ta = data
         sen.append(te)
         tag.append(ta)
+    all_text_tag = sorted(all_text_tag,key=lambda x:len(x[0]))
+    all_text = [x[0] for x in all_text_tag]
+    all_tag = [x[1] for x in all_text_tag]
     return all_text,all_tag
 def build_tag(all_data):
     word2idx = {"PAD":0,"UNK":100,"O":2}
@@ -33,26 +34,29 @@ def build_tag(all_data):
             word2idx[w] = word2idx.get(w,len(word2idx))
     return word2idx
 class NDataset(Dataset):
-    def __init__(self,all_text,all_tag,tokenizer,tag2idx,maxlen=None,is_dev=False):
+    def __init__(self,all_text,all_tag,tokenizer,tag2idx):
         self.all_text = all_text
         self.all_tag = all_tag
         self.tokenizer = tokenizer
         self.tag2idx = tag2idx
-        self.maxlen = maxlen
-        self.is_dev=is_dev
     def __getitem__(self, x):
         text,tag = self.all_text[x],self.all_tag[x]
-        if self.is_dev:
-            max_len = len(text)
-        else :
-            max_len = self.maxlen
-        text_idx = self.tokenizer.encode(text, add_special_tokens=True, truncation=True, max_length=max_len + 2,
-                              padding="max_length", return_tensors='pt').squeeze(0)
-        tag_idx = [0]+[self.tag2idx.get(i,100) for i in tag[:max_len]]+[0]
-        tag_idx += [0]*(len(text_idx)-len(tag_idx))
-
-        return text_idx,torch.tensor(tag_idx,dtype=torch.int64)
-
+        return text,tag,len(text)
+    def collate_fn(self,data):
+        batch_len = []
+        for d in data:
+            batch_len.append(d[2])
+        batch_max = max(batch_len)
+        batch_text, batch_tag = [],[]
+        for d in data:
+            text,tag = d[:2]
+            text_idx = self.tokenizer.encode(text, add_special_tokens=True, max_length=batch_max + 2,
+                                             padding="max_length")
+            tag_idx = [0] + [self.tag2idx.get(i, 100) for i in tag] + [0]
+            tag_idx += [0] * (len(text_idx) - len(tag_idx))
+            batch_text.append(text_idx)
+            batch_tag.append(tag_idx)
+        return torch.tensor(batch_text),torch.tensor(batch_tag,dtype=torch.int64),torch.tensor(batch_len,dtype=torch.int64)
     def __len__(self):
         return len(self.all_text)
 
@@ -69,7 +73,7 @@ class Bmodel(nn.Module):
         self.classifier = nn.Linear(768,class_num)
         self.loss_fn = nn.CrossEntropyLoss()
     def forward(self,x,label=None):
-        bert_out_0,bert_out_1 = self.bert(x,return_dict=False)
+        bert_out_0,bert_out_1 = self.bert(x,attention_mask=(x>0),return_dict=False)
         pre = self.classifier(bert_out_0)
         if label is not None:
             loss = self.loss_fn(pre.reshape(-1,pre.shape[-1]),label.reshape(-1))
@@ -82,22 +86,20 @@ if __name__ == "__main__":
     all_text, all_tag = get_data(os.path.join('..', 'data', 'ner', 'BIESO', 'train.txt'))
     dev_text, dev_tag = get_data(os.path.join('..', 'data', 'ner', 'BIESO', 'dev.txt'))
 
-    maxlen = 35
     epoch = 10
     lr = 0.00001
     batch_size = 20
 
     device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
-    # device = torch.device("cpu")
     tag2idx = build_tag(all_tag)
     idx2tag = list(tag2idx)
     tokenizer = BertTokenizer.from_pretrained(os.path.join('..','data','bert_base_chinese'))
 
-    train_dataset = NDataset(all_text,all_tag,tokenizer,tag2idx,maxlen)
-    train_dataloader = DataLoader(train_dataset,batch_size=batch_size,shuffle=True)
+    train_dataset = NDataset(all_text,all_tag,tokenizer,tag2idx)
+    train_dataloader = DataLoader(train_dataset,batch_size=batch_size,shuffle=False,collate_fn=train_dataset.collate_fn)
 
-    dev_dataset = NDataset(dev_text, dev_tag, tokenizer, tag2idx, is_dev=True)
-    dev_dataloader = DataLoader(dev_dataset, batch_size=1, shuffle=False)
+    dev_dataset = NDataset(dev_text, dev_tag, tokenizer, tag2idx)
+    dev_dataloader = DataLoader(dev_dataset, batch_size=1, shuffle=False,collate_fn=train_dataset.collate_fn)
 
 
     model = Bmodel(len(tag2idx)).to(device)
@@ -106,7 +108,7 @@ if __name__ == "__main__":
         loss_sum = 0
         ba = 0
         model.train()
-        for x,y in tqdm(train_dataloader):
+        for x,y,batch_len in tqdm(train_dataloader):
             x = x.to(device)
             y = y.to(device)
             opt.zero_grad()
@@ -119,12 +121,9 @@ if __name__ == "__main__":
 
         model.eval()
         all_pre = []
-        now_tag = []
-        for x, y in tqdm(dev_dataloader):
+        for x, y,batch_len in tqdm(dev_dataloader):
             x = x.to(device)
             pre = model(x).reshape(-1)
-            pre = [idx2tag[i] for i in pre]
+            pre = [idx2tag[i] for i in pre[1:batch_len+1]]
             all_pre.append(pre)
-            tagg = [idx2tag[i] for i in y.reshape(-1)]
-            now_tag.append(tagg)
-        print(f'f1_score={f1_score(all_pre, now_tag):.2f} precision_score={precision_score(all_pre, now_tag):.5f} recall_score={recall_score(all_pre, now_tag):.5f}')
+        print(f'f1_score={f1_score(all_pre, dev_tag):.2f} precision_score={precision_score(all_pre, dev_tag):.5f} recall_score={recall_score(all_pre, dev_tag):.5f}')
